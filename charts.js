23c4fe2062c36e7a59fb804c0aeb41efe8e9473e
sabedi5 <164125714+sabedi5@users.noreply.github.com>
2024-03-30 04:07:47 -0400

    Create charts.js

---
+++ charts.js
@@ -0,0 +1,25 @@
+// charts.js
+
+var ctx = document.getElementById('progressChart').getContext('2d');
+var progressChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+        datasets: [{
+            label: 'Workout Duration (minutes)',
+            data: [45, 60, 50, 55, 70, 65],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
